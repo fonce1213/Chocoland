0ac2ca88a47beba4744833baa341d4f7e61389e2
EC2 Default User <ec2-user@ip-172-31-5-47.ap-northeast-1.compute.internal>
2022-11-27 10:41:21 +0000

    [Add] Bootstrap, devise

--- README.md
+++ README.md
@@ -28,7 +28,7 @@
 ## 開発環境
 - OS：Linux(CentOS)
 - 言語：HTML,CSS,JavaScript,Ruby,SQL
-- フレームワーク：Ruby on Rails
+- フレームワーク：Ruby on Rails 6.1.7
 - JSライブラリ：jQuery
 - IDE：Cloud9
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # ユーザー用
+  # URL /users/...
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions",
+    passwords: "public/passwords"
+  }
+  
+  # 管理者用
+  # URL /admin/...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
